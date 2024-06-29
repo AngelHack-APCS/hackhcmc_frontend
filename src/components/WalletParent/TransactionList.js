0877c6e090edfd39ea2c1bf129e9046e519d0f22
dphu2609 <leducphu.login@gmail.com>
2024-06-29 23:56:21 +0700

    feat: complete ui parent wallet

--- src/components/WalletParent/TransactionList.js
+++ src/components/WalletParent/TransactionList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Transaction from "./Transaction"; // Make sure the path to the Transaction component is correct
 
-const transactions = [
+const historyTransactions = [
   // Add some sample transactions data here
   {
     id: 1,
@@ -25,12 +25,117 @@ const transactions = [
     amount: 50,
     content: "Payment sent",
   },
-  // Add more transactions as needed
+  {
+    id: 3,
+    icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/189021f56083de40e8866b0542a256913b68042ab15141a1aab65f2c23ea1fa3?apiKey=8b7bde825caa4a699878600964889e8b&",
+    date: "2024-06-28",
+    name: "Transaction 2",
+    type: "minus",
+    points: 50,
+    tran_code: "T2",
+    amount: 50,
+    content: "Payment sent",
+  },
+  {
+    id: 4,
+    icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/189021f56083de40e8866b0542a256913b68042ab15141a1aab65f2c23ea1fa3?apiKey=8b7bde825caa4a699878600964889e8b&",
+    date: "2024-06-28",
+    name: "Transaction 2",
+    type: "minus",
+    points: 50,
+    tran_code: "T2",
+    amount: 50,
+    content: "Payment sent",
+  },
+  {
+    id: 5,
+    icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/189021f56083de40e8866b0542a256913b68042ab15141a1aab65f2c23ea1fa3?apiKey=8b7bde825caa4a699878600964889e8b&",
+    date: "2024-06-28",
+    name: "Transaction 2",
+    type: "minus",
+    points: 50,
+    tran_code: "T2",
+    amount: 50,
+    content: "Payment sent",
+  },
 ];
 
-const TransactionList = ({ onTransactionClick }) => {
+const pendingTransactions = [
+    // Add some sample transactions data here
+    {
+      id: 1,
+      icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/189021f56083de40e8866b0542a256913b68042ab15141a1aab65f2c23ea1fa3?apiKey=8b7bde825caa4a699878600964889e8b&",
+      date: "2024-06-29",
+      name: "Transaction 1",
+      type: "minus",
+      points: 100,
+      tran_code: "T1",
+      amount: 100,
+      content: "Payment received",
+    },
+    {
+      id: 2,
+      icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/189021f56083de40e8866b0542a256913b68042ab15141a1aab65f2c23ea1fa3?apiKey=8b7bde825caa4a699878600964889e8b&",
+      date: "2024-06-28",
+      name: "Transaction 2",
+      type: "plus",
+      points: 50,
+      tran_code: "T2",
+      amount: 50,
+      content: "Payment sent",
+    },
+    {
+        id: 3,
+        icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/189021f56083de40e8866b0542a256913b68042ab15141a1aab65f2c23ea1fa3?apiKey=8b7bde825caa4a699878600964889e8b&",
+        date: "2024-06-29",
+        name: "Transaction 1",
+        type: "minus",
+        points: 100,
+        tran_code: "T1",
+        amount: 100,
+        content: "Payment received",
+      },
+      {
+        id: 4,
+        icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/189021f56083de40e8866b0542a256913b68042ab15141a1aab65f2c23ea1fa3?apiKey=8b7bde825caa4a699878600964889e8b&",
+        date: "2024-06-28",
+        name: "Transaction 2",
+        type: "plus",
+        points: 50,
+        tran_code: "T2",
+        amount: 50,
+        content: "Payment sent",
+      },
+      {
+        id: 5,
+        icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/189021f56083de40e8866b0542a256913b68042ab15141a1aab65f2c23ea1fa3?apiKey=8b7bde825caa4a699878600964889e8b&",
+        date: "2024-06-28",
+        name: "Transaction 2",
+        type: "plus",
+        points: 50,
+        tran_code: "T2",
+        amount: 50,
+        content: "Payment sent",
+      },
+      {
+        id: 6,
+        icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/189021f56083de40e8866b0542a256913b68042ab15141a1aab65f2c23ea1fa3?apiKey=8b7bde825caa4a699878600964889e8b&",
+        date: "2024-06-28",
+        name: "Transaction 2",
+        type: "plus",
+        points: 50,
+        tran_code: "T2",
+        amount: 50,
+        content: "Payment sent",
+      },
+  ];
+
+
+const TransactionList = ({selectedTab, onTransactionClick }) => {
+    const transactions = selectedTab === "history" ? historyTransactions : pendingTransactions;
+
   return (
-    <div className="overflow-y-auto">
+    <div className="overflow-y-auto max-h-[450px]">
       {transactions.map((trans) => (
         <div key={trans.id} onClick={() => onTransactionClick(trans)}>
           <Transaction trans={trans} />
